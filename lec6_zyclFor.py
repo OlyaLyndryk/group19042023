9bf7116eb0b27d0505b8613515aeb8ba382d632c
lab 001 <olya3311@gmail.com>
2023-05-10 23:43:44 +0200

    add lec6_for

---
+++ lec6_zyclFor.py
@@ -0,0 +1,136 @@
+# my_text = 'I love python'
+# # print(id(my_text))
+# # my_text += ' very much'
+# # print(id(my_text))
+# #
+# # some_new = my_text.split()
+# # print(some_new)
+# # print(type(some_new))
+#
+# # my_text = 'I love\npython'
+# # print(id(my_text))
+# # my_text += ' very \t\t\t\tmuch'
+# # print(id(my_text))
+# # print(my_text)
+# #
+# # some_new = my_text.split()
+# # print(some_new)
+# # print(type(some_new))
+#
+# # new_list = ['', 5, 3.3, 'data', [6, 56]]
+# # new_list_2 = []
+# # new_list_3 = list()
+# #
+# # lenght_list = len(new_list)
+# #
+# # print(bool(new_list))
+# # print(bool(new_list_2))
+# #
+# # if new_list:
+# #     print('new list')
+# #
+# # print(55555555555)
+# # pass
+#
+# # new_list = ['', 5, 3.3, 'data', [6, 56]]
+# # new_list_2 = [5555555555]
+# # new_list_3 = list()
+# #
+# # lenght_list = len(new_list)
+# #
+# # new_list.append('one')
+# # print(new_list)
+#
+#
+#
+# # new_list = [1, 5, 3.3, 6666, 9999, -6]
+# # new_list_2 = [5555555555]
+# # new_list_3 = list()
+# #
+# # lenght_list = len(new_list)
+# #
+# # new_list.append(1_000_000)
+# # print(new_list)
+# # new_list.sort()
+# # print(new_list)
+#
+#
+#
+# new_list = [1, 5, 3.3, 5, -3]
+# new_list_2 = [5555555555]
+# new_list_3 = list()
+#
+# lenght_list = len(new_list)
+#
+# new_list.append(10)
+# print(id(new_list))
+# print(new_list)
+# new_list.sort(reverse=True)
+# print(new_list)
+#
+# print(new_list[0])
+# print(new_list[5])
+#
+# for temperature_value in new_list:
+#     print(temperature_value, end='>>> ')
+#     if temperature_value > 0:
+#         print('warm')
+#     else:
+#         print('cold')
+#
+# #
+# #
+# #
+# #
+# # for letter in my_text:
+# #     print(letter)
+# #
+# #
+# # for letter in ['5jjjj', '67']:
+# #     print(letter)
+# #
+# # for letter in my_text:
+# #     print(type(letter))
+#
+# # print(range(31))
+# # for day in range(31):
+# #     print(day)
+# #
+# #
+# # print(range(1, 32))
+# # for day in range(1, 32):
+# #     print(day)
+#
+# # new_list.clear()
+# # new_list = []
+# # print(new_list)
+#
+# # MAX_TEMP = 50
+# # MIN_TEMP = -40
+# #
+# # for day in range(1, 3 + 1):
+# #     print(day)
+# #     temp = float(input(f'day {day}: enter temperature >>> '))
+# #     new_list.append(temp)
+# #
+# # print(new_list)
+# # if new_list:
+# #     print(sum(new_list))
+#
+#
+#
+#
+# MAX_TEMP = 50
+# MIN_TEMP = -40
+#
+# for day in range(1, 3 + 1):
+#     print(day)
+#     temp = float(input(f'day {day}: enter temperature >>> '))
+#     if temp < MIN_TEMP or temp > MAX_TEMP:
+#         break
+#     new_list.append(temp)
+#
+# print(new_list)
+# if new_list:
+#     print(sum(new_list)/(new_list))
+
